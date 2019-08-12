8571a81151696375d14594a3193d8c2bb894064d
sagarCN <sagar.gupta@codenation.co.in>
2019-08-12 12:59:44 +0400

    Remove commented code

--- project/serializers.py
+++ project/serializers.py
@@ -8,25 +8,14 @@ class CategoriesSerializer(serializers.ModelSerializer):
         model = Category
         fields = ('id', 'name')
 
-#
-# class ExpenseInfoSerializer(serializers.HyperlinkedModelSerializer):
-#     id = serializers.ReadOnlyField(source='User.id')
-#
-#     class Meta:
-#         model = ExpenseInfo
-#         fields = ('id', 'owe', 'lend')
-        # read_only_fields = ('expense_id')
-#
-#
-# class ExpenseSerializer(serializers.ModelSerializer):
-#     members = ExpenseInfoSerializer(source='expenseinfo_set', many=True)
-#
-#     class Meta:
-#         model = Expense
-#         fields = ('id', 'categories_id', 'total_amount', 'description', 'members')
+
+class ExpenseInfoSerializer(serializers.HyperlinkedModelSerializer):#
+    class Meta:
+        model = ExpenseInfo
+        fields = ('id', 'owe', 'lend')
 
 
-    # def create(self, validated_data):
-    #     return Expense.objects.create(**validated_data)
-
-
+class ExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expense
+        fields = ('id', 'categories_id', 'total_amount', 'description')
